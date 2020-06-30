c153e33aa9fe243d571074ba3b9927b125d7667f
Shiobart <h070114261@gmail.com>
2020-06-30 22:45:09 +0900

    Revert ".jsの初期作成"
    This reverts commit 0614bf8710d59abd02ed3fe73eadce71a9bb7309.

--- index.js
+++
@@ -1,6 +0,0 @@
-function Add(){
-    var txtLeftSide = document.getElementById("txtLeftSide");
-    var txtRightSide = document.getElementById("txtRightSide");
-    var output = document.getElementById("output");
-    output.innerHTML = Number(txtLeftSide.value) + Number(txtRightSide.value);
-}
